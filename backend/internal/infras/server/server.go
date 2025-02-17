c6e5d09be3cae4f4a813c0fc15b9581b2c579391
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-17 15:48:46 +0700

    change position logger

--- backend/internal/infras/server/server.go
+++ backend/internal/infras/server/server.go
@@ -27,7 +27,6 @@ func AppServer() *Server {
 
 func (s *Server) routes(prefix string, version string, db *gorm.DB) {
 	url := fmt.Sprintf("/%s/%s", prefix, version)
-	s.app.Use(fiberlog.New())
 
 	mainGroup := s.app.Group(url)
 
@@ -38,6 +37,8 @@ func (s *Server) routes(prefix string, version string, db *gorm.DB) {
 }
 
 func (s *Server) Start() {
+	s.app.Use(fiberlog.New())
+
 	env := config.LoadEnv()
 
 	db := database.Connect(env)
