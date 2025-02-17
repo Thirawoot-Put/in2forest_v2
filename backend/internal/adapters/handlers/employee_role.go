75659b9c766aab09b15989d8b7b22b58dbab7faf
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-17 16:36:16 +0700

    refactor

--- backend/internal/adapters/handlers/employee_role.go
+++ backend/internal/adapters/handlers/employee_role.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 	"thirawoot/in2forest_shop_backend/internal/dto"
 	portin "thirawoot/in2forest_shop_backend/internal/ports/port_in"
+	"thirawoot/in2forest_shop_backend/internal/utils/constants"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -32,7 +33,7 @@ func (s *EmployeeRoleHandler) Delete(c *fiber.Ctx) error {
 	idStr := c.AllParams()["id"]
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		return fiber.NewError(fiber.ErrBadRequest.Code, "failed to convert id, make sure it's integer")
+		return fiber.NewError(constants.StatusCode.BadRequest, "failed to convert id, make sure it's integer")
 	}
 
 	result := s.service.Delete(uint(id))
