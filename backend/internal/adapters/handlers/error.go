da870f5b9a432e8fed8a797c6388b0ccf70783b8
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-18 17:19:33 +0700

    upgrade error handler

--- backend/internal/adapters/handlers/error.go
+++ backend/internal/adapters/handlers/error.go
@@ -1,14 +1,14 @@
 package handlers
 
 import (
-	"fmt"
+	"log"
 	"thirawoot/in2forest_shop_backend/internal/application"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func HandleAppErr(e error, c *fiber.Ctx) error {
-	fmt.Println(e)
+	log.Printf("Error occurred in %s %s: %v \n", c.Method(), c.Path(), e)
 
 	if appErr, ok := e.(*application.AppErr); ok {
 		return c.Status(appErr.Code).JSON(fiber.Map{
