82cf3b540dcc81546a70d0d88449e3ddcfe3c30e
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-18 14:07:14 +0700

    create with hash password

--- backend/internal/application/employee.go
+++ backend/internal/application/employee.go
@@ -5,6 +5,7 @@ import (
 	"thirawoot/in2forest_shop_backend/internal/dto"
 	portin "thirawoot/in2forest_shop_backend/internal/ports/port_in"
 	portout "thirawoot/in2forest_shop_backend/internal/ports/port_out"
+	"thirawoot/in2forest_shop_backend/internal/utils/bcrypt"
 	"thirawoot/in2forest_shop_backend/internal/utils/constants"
 )
 
@@ -19,9 +20,14 @@ func NewEmployeeApp(repository portout.EmployeeRepository) portin.EmployeeApp {
 }
 
 func (s *EmployeeAppImpl) Create(data dto.Employee) (*dto.Employee, error) {
+	hash, err := bcrypt.HashPwd(data.Password)
+	if err != nil {
+		return nil, NewAppErr("FAILED_HASH_PASSWORD", constants.Code.ServerError, err)
+	}
+
 	result, err := s.repo.Create(&domain.Employee{
 		Email:    data.Email,
-		Password: data.Password,
+		Password: hash,
 		Name:     data.Name,
 		Mobile:   data.Mobile,
 	})
