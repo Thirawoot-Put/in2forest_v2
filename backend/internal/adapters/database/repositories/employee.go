bb7b87d33a0d468ca7e317e06f9b4d86e58a32c6
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-19 18:01:51 +0700

    test some assume

--- backend/internal/adapters/database/repositories/employee.go
+++ backend/internal/adapters/database/repositories/employee.go
@@ -2,7 +2,6 @@ package repositories
 
 import (
 	"errors"
-	"fmt"
 	"thirawoot/in2forest_shop_backend/internal/domain"
 	"thirawoot/in2forest_shop_backend/internal/infras/database/model"
 	portout "thirawoot/in2forest_shop_backend/internal/ports/port_out"
