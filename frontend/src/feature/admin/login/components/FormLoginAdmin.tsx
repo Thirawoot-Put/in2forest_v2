3dc3b9d197835c25726bc10313cb8f3d7169933a
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-24 18:03:55 +0700

    prepare for button

--- frontend/src/feature/admin/login/components/FormLoginAdmin.tsx
+++ frontend/src/feature/admin/login/components/FormLoginAdmin.tsx
@@ -4,7 +4,7 @@ import { TextField } from "@mui/material";
 
 export default function FormLoginAdmin() {
   return (
-    <form className="flex flex-col gap-2 items-center w-full h-full">
+    <form className="flex flex-col gap-2 items-center w-full h-full py-4 ">
       <TextField className="w-4/6" label="Username" size="small" id="outlined-size-normal" placeholder="sample@mail.com" />
       <TextField className="w-4/6" label="Password" size="small" id="outlined-size-normal" type="password" placeholder="password1234" />
     </form>
--- frontend/src/feature/admin/login/index.tsx
+++ frontend/src/feature/admin/login/index.tsx
@@ -3,7 +3,7 @@ import FormLoginAdmin from "./components/FormLoginAdmin";
 export default function FeatureAdminLogin() {
   return (
     <div className="w-10/12 border border-gray-500 rounded-lg py-6">
-      <p className="pb-4 font-semibold text-xl text-center">Admin Login</p>
+      <p className="pb-2 font-semibold text-xl text-center">Admin Login</p>
       <FormLoginAdmin />
     </div>
   )
