64174e2a5446b1a0e61587aae33f42861b504bb2
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-24 18:02:30 +0700

    use padding

--- frontend/src/feature/admin/login/index.tsx
+++ frontend/src/feature/admin/login/index.tsx
@@ -2,8 +2,8 @@ import FormLoginAdmin from "./components/FormLoginAdmin";
 
 export default function FeatureAdminLogin() {
   return (
-    <div className="flex flex-col gap-4 w-10/12 border border-gray-500 rounded-lg py-6">
-      <p className="font-semibold text-xl text-center">Admin Login</p>
+    <div className="w-10/12 border border-gray-500 rounded-lg py-6">
+      <p className="pb-4 font-semibold text-xl text-center">Admin Login</p>
       <FormLoginAdmin />
     </div>
   )
